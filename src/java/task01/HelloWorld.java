3c4ff75dbfcbe1e04fef3c349a10bb07d7c00d81
Janna Gorelova <Janny.Gorelova@gmail.com>
2017-06-20 18:16:00 +0300

    - main method has been implemented

---
+++ src/java/task01/HelloWorld.java
@@ -0,0 +1,7 @@
+package task01;
+
+public final class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" Hello World! ");
+    }
+}
--- src/java/task_01/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package java.task_01;
-
-
-public final class HelloWorld {
-}
