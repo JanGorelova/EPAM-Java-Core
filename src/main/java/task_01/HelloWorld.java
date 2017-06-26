4727d022509aab4c1bfe3469c341a4762b6739ea
Жанна <Janny.Gorelova@gmail.com>
2017-06-26 12:58:29 +0300

    - Testing git

--- src/main/java/task_01/HelloWorld.java
+++ src/main/java/task_01/HelloWorld.java
@@ -2,6 +2,6 @@ package task_01;
 
 public final class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(" Hello World! ");
+        System.out.println("Hello World!");
     }
 }
