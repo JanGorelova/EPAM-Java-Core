8a07c44811302d603654ab70185c1e08c8a6e00f
Janna Gorelova <Janny.Gorelova@gmail.com>
2017-06-27 13:03:56 +0300

    - constructors for Amazing and Rubber ducks has been created

--- src/main/java/duckCasino/ducks/AmazingDuck.java
+++ src/main/java/duckCasino/ducks/AmazingDuck.java
@@ -1,17 +1,13 @@
 package duckCasino.ducks;
 
 import duckCasino.Duck;
-import duckCasino.behaviors.FlyBehavior;
-import duckCasino.behaviors.FlyingDuckBehaviour;
-import duckCasino.behaviors.QuackBehavior;
+import duckCasino.behaviors.*;
 
 public final class AmazingDuck extends Duck {
 
     public AmazingDuck() {
-        super(new FlyingDuckBehaviour(), new QuackBehavior() {
-            @Override
-            public void quack() {
-            }
-        });
+        super(new FlyingDuckBehaviour(), new QuackingBehavior());
+
+        }
     }
-}
+
--- src/main/java/duckCasino/ducks/RubberDuck.java
+++ src/main/java/duckCasino/ducks/RubberDuck.java
@@ -3,11 +3,12 @@ package duckCasino.ducks;
 import duckCasino.Duck;
 import duckCasino.behaviors.FlyBehavior;
 import duckCasino.behaviors.NoFlyingDuckBehaviour;
+import duckCasino.behaviors.NoQuackingBehavior;
 import duckCasino.behaviors.QuackBehavior;
 
 public final class RubberDuck extends Duck {
 
-    public RubberDuck(NoFlyingDuckBehaviour flyBehavior, QuackBehavior quackBehavior) {
-        super(flyBehavior,quackBehavior);
+    public RubberDuck() {
+        super(new NoFlyingDuckBehaviour(), new NoQuackingBehavior());
     }
 }
