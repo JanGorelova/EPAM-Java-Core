a227f30d067a1df75266eabb461760da70da22ec
Жанна <Janny.Gorelova@gmail.com>
2017-06-29 16:37:22 +0300

    - duckNumber exception has been added

--- src/main/java/duckCasino/Command.java
+++ src/main/java/duckCasino/Command.java
@@ -6,6 +6,8 @@ public final class Command {
     private final int duckNumber;
 
     public Command(final int duckNumber, final int bet) {
+        if (duckNumber <=0 || duckNumber > 5)
+            throw new RuntimeException("Duck number maust be from 1 to 5!");
         this.bet = bet;
         this.duckNumber = duckNumber;
     }
