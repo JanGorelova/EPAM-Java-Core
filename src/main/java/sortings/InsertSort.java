c008e52f8e313bee4125203433a27da95fdd6602
Жанна <Janny.Gorelova@gmail.com>
2017-07-21 14:37:44 +0300

    - InsertSord class methds sort, printArray , isLess have been implemented

--- src/main/java/sortings/InsertSort.java
+++ src/main/java/sortings/InsertSort.java
@@ -1,14 +1,24 @@
 package sortings;
 
+import java.util.Comparator;
 import java.util.Random;
 
 public final class InsertSort {
     public static void main(String[] args) {
-
+        final int[] array = randomizeFiller(100);
+        printArray(array);
+        sort(array);
+        printArray(array);
     }
 
     private static void sort(final int[] array) {
+        final int N = array.length;
 
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0 && isLess(array[j], array[j-1]) ; j--) {
+                swap(array,j,j-1);
+            }
+        }
     }
 
     private static void swap(final int[] array, final int i, final int j) {
@@ -27,5 +37,22 @@ public final class InsertSort {
         }
 
         return array;
+    }
+
+    private static boolean isLess(final Comparable first, final Comparable second) {
+        return first.compareTo(second) < 0;
+
+    }
+
+    private static final void printArray(final int[] array) {
+        final StringBuilder builder = new StringBuilder(" [");
+
+        for (int i = 0; i < array.length; i++) {
+            if (i != array.length - 1) {
+                builder.append(array[i]).append(", ");
+            } else builder.append(array[i]).append(" ]");
+        }
+        System.out.println();
+        System.out.println(builder);
     }
 }
