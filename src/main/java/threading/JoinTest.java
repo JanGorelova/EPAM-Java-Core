8b6ff144b949eae4eb2beb16ef0ecdc9185c0332
Жанна <Janny.Gorelova@gmail.com>
2017-08-20 14:00:36 +0300

    javadoc JoinTest

--- src/main/java/threading/JoinTest.java
+++ src/main/java/threading/JoinTest.java
@@ -1,6 +1,12 @@
 package threading;
 
 public final class JoinTest {
+    /**
+     * tests the join key word
+     *
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(final String[] args) throws InterruptedException {
         Thread thread = new Thread(new Runnable() {
             public void run() {
