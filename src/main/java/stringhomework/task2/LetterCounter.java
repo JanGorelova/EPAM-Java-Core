d68dbc02fff83211f66f0c16e83a8223a2d735a1
Жанна <Janny.Gorelova@gmail.com>
2017-08-20 13:11:15 +0300

    javadoc LetterCounter

--- src/main/java/stringhomework/task2/LetterCounter.java
+++ src/main/java/stringhomework/task2/LetterCounter.java
@@ -12,6 +12,12 @@ public final class LetterCounter {
         System.out.println(counter.getLettersCount(counter.readWord()));
     }
 
+    /**
+     * shows the number of the different letters in word
+     *
+     * @param word to be analyzed
+     * @return the number of different letters in specified word
+     */
     private int getLettersCount(final String word) {
         final HashSet<Character> characters = new HashSet<>();
         for (int i = 0; i < word.length(); i++)
@@ -20,16 +26,21 @@ public final class LetterCounter {
         return characters.size();
     }
 
+    /**
+     * prints the word written in console
+     *
+     * @return the string written in console
+     * @throws RuntimeException if user did not enter the word
+     */
     private String readWord() {
         System.out.println("Please, put the word");
-        String word = "You did not put the word!";
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            word = reader.readLine();
+            String word = reader.readLine();
+            return word;
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return word;
+        throw new RuntimeException("You did not put the word!");
     }
 }
