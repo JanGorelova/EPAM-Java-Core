b42f04dafe252a1808040c93007bed4dd49e34d0
Жанна <Janny.Gorelova@gmail.com>
2017-08-21 16:48:06 +0300

    javadoc Command

--- src/main/java/duckcasino/behaviors/quack/QuackBehavior.java
+++ src/main/java/duckcasino/behaviors/quack/QuackBehavior.java
@@ -3,9 +3,6 @@ package duckcasino.behaviors.quack;
 public interface QuackBehavior {
     /**
      * Receive quackBehaviour
-     *
-     * @param
-     * @return quackBehaviour
      */
 
     public void quack();
--- src/main/java/duckcasino/console/Command.java
+++ src/main/java/duckcasino/console/Command.java
@@ -7,6 +7,12 @@ public final class Command {
     private final int bet;
     private final int duckNumber;
 
+    /**
+     * creates new command object
+     *
+     * @param duckNumber to take part in competition
+     * @param bet to be in the game
+     */
     public Command(final int duckNumber, final int bet) {
         if (duckNumber <= 0 || duckNumber > Constants.DUCKS_COUNT)
             throw new RuntimeException("Duck number must be from 1 to 5!");
