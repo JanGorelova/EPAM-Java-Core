3b53e0d436f18fc876051538b3eb84e368c6d305
Жанна <Janny.Gorelova@gmail.com>
2017-08-21 17:00:32 +0300

    javadoc DuckCompetition

--- src/main/java/duckcasino/DuckCompetition.java
+++ src/main/java/duckcasino/DuckCompetition.java
@@ -12,25 +12,40 @@ public final class DuckCompetition {
     private final Duck[] ducks;
     private final Random random = new Random();
 
+    /**
+     * creates new competition with specified duck number
+     *
+     * @param duckNumber take part in competition
+     */
     public DuckCompetition(final int duckNumber) {
         this.ducks = new Duck[duckNumber];
-        for (int i = 0; i < duckNumber ; i++)
-            ducks[i]= getRandomDuck();
+        for (int i = 0; i < duckNumber; i++)
+            ducks[i] = getRandomDuck();
     }
 
-    private final Duck getRandomDuck() {
+    /**
+     * creates on of two duck types
+     *
+     * @return random Duck
+     */
+    private Duck getRandomDuck() {
         if (random.nextBoolean())
             return new AmazingDuck();
         else
             return new RubberDuck();
     }
 
+    /**
+     * find winner in competition
+     *
+     * @return number of the winner
+     */
     public final int findWinner() {
         int distance = 0;
         int winnerNumber = 0;
-        for (int i = 0; i< ducks.length; i++) {
+        for (int i = 0; i < ducks.length; i++) {
             final int currentDuckDistance = ducks[i].performFly(COMPETITION_TIME);
-            if ( currentDuckDistance > distance) {
+            if (currentDuckDistance > distance) {
                 distance = currentDuckDistance;
                 winnerNumber = i;
             }
